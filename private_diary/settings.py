b69e7888127e6c3c33af65409ac49961d1c58747
niwayama <niwayama.takeyuki@b-architects.com>
2020-11-21 13:43:13 +0900

    finished Chapter 7 in Django Developing

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 #Logging Configure
 LOGGING ={
     'version': 1, #1固定
