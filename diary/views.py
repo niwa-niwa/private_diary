0125d1dc454575f55cc83e498f14652ab958e817
niwayama <niwayama.takeyuki@b-architects.com>
2020-11-22 21:21:26 +0900

    made createview.py in Diary

--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django .views import generic
-from .forms import InquiryForm
+from .forms import InquiryForm, DiaryCreateForm
 import logging
 from django.urls import reverse_lazy
 from django.views import generic
@@ -42,3 +42,21 @@ class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
   model = Diary
   template_name = 'diary_detail.html'
   # pk_url_kwarg = 'id' # オーバーライドさせるとエラーが出るのでコメントアウト
+
+
+class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
+  model = Diary
+  template_name = 'diary_create.html'
+  form_class = DiaryCreateForm
+  success_url = reverse_lazy('diary:diary_list')
+
+  def form_valid(self, form):
+    diary = form.save(commit=False)
+    diary.user = self.request.user
+    diary.save()
+    messages.success(self.request, '日記を作成しました。')
+    return super().form_valid(form)
+
+  def form_invalid(self, form):
+    messages.error(self.request, "日記の作成に失敗しました。")
+    return super().form_invalid(form)
